11a41464fa76a9f782ad6ee172be0bad820464f8
Ana Pamela Osuna Vargas <osuna@login01.cm.cluster>
2024-02-19 15:29:25 +0100

    custom dataset

--- src/trackformer/datasets/tracking/factory.py
+++ src/trackformer/datasets/tracking/factory.py
@@ -8,6 +8,7 @@ from torch.utils.data import ConcatDataset
 
 from .demo_sequence import DemoSequence
 from .mot_wrapper import MOT17Wrapper, MOT20Wrapper, MOTS20Wrapper
+from .spine_sequence import SpineSequence
 
 DATASETS = {}
 
@@ -33,6 +34,10 @@ for split in ['TRAIN', 'TEST', 'ALL', '01', '02', '05', '06', '07', '09', '11', 
     name = f'MOTS20-{split}'
     DATASETS[name] = (
         lambda kwargs, split=split: MOTS20Wrapper(split, **kwargs))
+    
+for split in ['val']:
+    name = f'spine-{split}'
+    DATASETS[name] = (lambda kwargs: [SpineSequence(split, **kwargs), ])
 
 DATASETS['DEMO'] = (lambda kwargs: [DemoSequence(**kwargs), ])
 
@@ -58,6 +63,19 @@ class TrackDatasetFactory:
         for dataset in datasets:
             assert dataset in DATASETS, f"[!] Dataset not found: {dataset}"
 
+
+            # if dataset == 'aid052N1D1_tp1_stack2':
+            #     data_root_dir = 'data/spine/aid052N1D1_tp1_stack2'
+            # else:
+            #     data_root_dir = kwargs['data_root_dir']
+            # img_transform = kwargs['img_transform']
+
+            # print(datasets)
+            # print(f'KWARGS: {kwargs}')
+            # if self._data is None:
+            #     self._data = DATASETS[dataset]({'root_dir': data_root_dir, 'img_transform': img_transform})
+            # else:
+            #     self._data = ConcatDataset([self._data, DATASETS[dataset]({'root_dir': data_root_dir, 'img_transform': img_transform})])
             if self._data is None:
                 self._data = DATASETS[dataset](kwargs)
             else:
